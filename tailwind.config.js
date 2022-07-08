5e3eb254db323bab66a4a863e1aa287085abf385
s4malve <s4malve@gmail.com>
2022-07-07 23:04:12 -0500

    build(tailwind): linear gradients added

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,14 @@ module.exports = {
         primary: '#861546',
         whatsapp: '#25d366'
       },
+      backgroundImage: {
+        section:
+          'linear-gradient( 104.16deg, #fafafa 2.62%, rgba(255, 255, 255, 0) 55.49%, #fafafa 87%)',
+        'comments-mobile':
+          'linear-gradient( 180deg, rgba(21, 26, 44, 0.65) 0%, rgba(21, 26, 44, 0) 84.18%)',
+        'comments-desktop':
+          'linear-gradient( 90deg, rgba(21, 26, 44, 0.65) 0%, rgba(21, 26, 44, 0) 84.18%)'
+      },
     }
   },
   plugins: []
