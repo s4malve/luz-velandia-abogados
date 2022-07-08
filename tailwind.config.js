c061049fab3f2ec343e65b93fe4248eb7e931f10
s4malve <s4malve@gmail.com>
2022-07-07 23:02:09 -0500

    build(tailwind): colors variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
       fontFamily: {
         body: ['Montserrat', 'sans-serif']
       },
+      colors: {
+        'dark-72': 'rgba(21, 26, 44, 0.72)',
+        'expressionism-green': '#53bf9d',
+        'golden-promise': '#f6a504',
+        'primary-75': '#861546c0',
+        'primary-90': '#841545e6',
+        'primary-95': '#841545f2',
+        'pucker-up': '#201a1b',
+        'spinel-red': '#ff4cbf',
+        brilliance: '#fdfdfd',
+        dark: '#151a2c',
+        gmail: '#ea4335',
+        primary: '#861546',
+        whatsapp: '#25d366'
+      },
     }
   },
   plugins: []
