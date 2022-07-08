6d3426a70fd2b0524345eb343c0705235f82b79b
s4malve <s4malve@gmail.com>
2022-07-07 23:26:35 -0500

    feat(data): dummy data for areas created

---
+++ data/areas.ts
@@ -0,0 +1,28 @@
+import Group from '../components/Icons/Group'
+
+export default [
+  {
+    href: '/areas/familia',
+    btn: 'btn-family',
+    title: 'Familia',
+    description: 'Lorem ipsum dolor sit amet, consectetur.',
+    color: '#ffc54d',
+    tablet: Group
+  },
+  {
+    href: '/areas/circuito',
+    title: 'Circuito',
+    btn: 'btn-circuit',
+    description: 'Lorem ipsum dolor sit amet, consectetur.',
+    color: '#ff4cbf',
+    tablet: Group
+  },
+  {
+    href: '/areas/civil',
+    title: 'Civil',
+    btn: 'btn-civil',
+    description: 'Lorem ipsum dolor sit amet, consectetur.',
+    color: '#53BF9D',
+    tablet: Group
+  }
+]
