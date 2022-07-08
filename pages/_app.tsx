9ced976fea00dd5523b074b156d12ee2c1d49142
s4malve <s4malve@gmail.com>
2022-07-08 12:23:08 -0500

    feat(pages): add _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
+import BaseHead from '../components/Head/BaseHead'
+import {
+  BASE_PAGE_COPY,
+  BASE_PAGE_TITLE,
+  BASE_PAGE_URL
+} from '../lib/constants'
+import '../styles/font-face.css'
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <BaseHead
+          title={BASE_PAGE_TITLE}
+          description={BASE_PAGE_COPY}
+          permalink={BASE_PAGE_URL}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
