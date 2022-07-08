cdb503520cec1041174331f3d814f396340bc116
s4malve <s4malve@gmail.com>
2022-07-08 13:18:04 -0500

    fix(components): add missing shareLinks props at article body

--- components/Article/Body.tsx
+++ components/Article/Body.tsx
@@ -1,17 +1,22 @@
 import { ReactElement } from 'react'
-import Aside from '../Aside'
+
 import markdownStyles from './markdown.module.css'
+
+import Aside from '../Aside'
+
+import ShareLinks from '../../types/share-links'
 
 type Props = {
   children?: ReactElement<any, any>
+  shareLinks: ShareLinks
 }
 
-const Body = ({ children }: Props) => {
+const Body = ({ children, shareLinks }: Props) => {
   return (
     <div className={`wrapper ${markdownStyles['markdown']}`}>
       <article className="content-container grid grid-cols-1 grid-rows-[auto_1fr] lg:grid-rows-[1fr] lg:grid-cols-[repeat(2,auto)] gap-6 justify-between">
         <main className="col-start-1 max-w-[700px]">{children}</main>
-        <Aside className="row-start-1 lg:row-auto" />
+        <Aside shareLinks={shareLinks} className="row-start-1 lg:row-auto" />
       </article>
     </div>
   )
